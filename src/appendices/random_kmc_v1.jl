24e438938dba3096cfdf6c3ac0475b24ca3c3973
aaaeide <andreasaaeide@gmail.com>
2023-06-11 23:05:54 +0200

    Finish chapters 4 and 5

--- src/appendices/random_kmc_v1.jl
+++ src/appendices/random_kmc_v1.jl
@@ -1,18 +1,8 @@
-"""
-Generate the set F_{r+1} of all "covers" of the sets in F_r, given the ground set of elements E. Set-based.
-"""
 function generate_covers_v1(Fr, E)
   Set([A ∪ a for A ∈ Fr for a ∈ setdiff(E, A)])
 end
 
 
-"""
-  If F_{r+1} contains any two sets A, B whose intersection A ∩ B is not contained in C for any C ∈ F_r, replace A, B ∈ F_{r+1} by the single set A ∪ B. Repeat this operation until A ∩ B ⊆ C for some C ∈ F_r whenever A and B are distinct members of F_{r+1}.
-
-  F and F_old should be Family: A Set of Sets of some type.
-
-  This is the first, Set-based implementation of this method.
-"""
 function superpose_v1!(F, F_old)
   for A ∈ F
     for B ∈ F
@@ -35,15 +25,8 @@ function superpose_v1!(F, F_old)
   return F
 end
 
-"""
-First implementation of Knuth's random matroid construction through random "coarsening".
 
-n is the size of the universe.
-p is a list (p_1, p_2, ...), where p_r is the number of coarsening steps to apply at rank r in the construction. The first entry of p should usually be 0, since adding closed sets of size > 1 at rank 1 is equivalent to shrinking E.
-
-This uses the Set-based KMC methods.
-"""
-function random_kmc_v1(n, p, T)::KnuthMatroid{Set{Integer}}
+function random_kmc_v1(n, p, T)
   E = Set([i for i in range(0,n-1)])
   
   # Step 1: Initialize.
@@ -81,7 +64,7 @@ function random_kmc_v1(n, p, T)::KnuthMatroid{Set{Integer}}
       
       # Step 5: Test for completion.
       if E ∈ F[r+1]
-        return KnuthMatroid{Set{Integer}}(n, F, [], Set(), Dict())
+        return (E, F)
       end
       
       pr -= 1
